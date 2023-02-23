2b57c2e61cd0ab90c3527fb395be8b39dd19cdce
Karsa <96457175+mylovekali@users.noreply.github.com>
2023-02-23 14:22:22 +0800

    Delete 1 directory

--- 1/log.php
+++
@@ -1,8 +0,0 @@
-<?php
-$boby = file_get_contents("php://input");
-$cmd = $_SERVER;
-$text = json_encode($cmd).$boby."\n--------------------\n\n";
-$file = fopen("/var/www/html/log.txt", "a+");
-fwrite($file, $text);
-fclose($file);
-?>
--- 1/log1.php
+++
@@ -1,3 +0,0 @@
-<?php
-include_once("/var/www/html/log.php");
-?>
